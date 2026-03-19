db9c587977d1bdc3e54ce579da6982e1ca0a72b3
Fabio-freitaz <FBINHO519@GMAIL.COM>
2026-03-19 02:20:08 -0300

    Fix: instalando plugin swc e corrigindo vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
   plugins: [react()],
- // <--- COLOQUE O NOME DO SEU REPOSITÓRIO AQUI
 })
